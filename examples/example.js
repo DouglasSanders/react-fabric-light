e47c0d68c9e1874bc08a0151a60528aff7de8c17
DouglasSanders <douglas.johman.sanders@gmail.com>
2018-07-18 14:19:35 +0800

    Slim Down Dependencies

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,9 @@
-var exports = {};
-
 requirejs.config({
   baseUrl: '../',
   packages: ['build', { name: 'build', main: 'fabriclight' }],
   paths: {
-    react: 'https://unpkg.com/react@16/umd/react.development',
-    reactDOM: 'https://unpkg.com/react-dom@16/umd/react-dom.development'
+    react: 'https://unpkg.com/react@16/umd/react.production.min',
+    reactDOM: 'https://unpkg.com/react-dom@16/umd/react-dom.production.min'
   }
 });
 
@@ -17,8 +15,11 @@ require(['require', 'react', 'reactDOM', 'build'], function(require) {
 
   class Example extends React.Component {
     render() {
-      var img = React.createElement('img', { src: 'sample.jpg' }, null);
-      return React.createElement(FabricLight, null, img);
+      return React.createElement(
+        FabricLight,
+        { maxWidth: 800, maxHeight: 500 },
+        React.createElement('img', { src: 'sample.jpg' }, null)
+      );
     }
   }
 
